4c00af1057a5a50d9ea88b61eb3e714cc95fb098
itsreisv <123587174+itsreisv@users.noreply.github.com>
2023-03-25 14:24:59 -0400

    finished project

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Welcome to my calculator project~!
 Pseudocode:
-// create a function "operate" that takes an operator and 2 numbers and calls a math function to them
-// 
+// create a function "operate" that takes an operator and 2 numbers and calls a math function to them - DONE
+// create a function that populates the display with the numbers pressed - DONE
+// figure out how to get multiple numbers to enter input at once
+// create a function for = called evaluate that gets an initial number input, then gets an operator, then gets a second number input 
--- main.js
+++ main.js
@@ -1,7 +1,74 @@
-let defaultNumberOne = 0;
-let defaultNumberTwo = 0;
-let firstOperand = '';
+const input = document.querySelector('.input');
+const lastInput = document.querySelector('.lastInput');
+const numbers = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+const decimal = document.querySelector('.dot');
+const clear = document.querySelector('.clear');
+const erase = document.querySelector('.delete');
 
+let firstNum = '';
+let secondNum = '';
+let defaultValue = 0;
+let firstOperator = '';
+let currentOperation = null;
+
+
+clear.addEventListener('click', clearPage)
+equals.addEventListener('click' , evaluate)
+erase.addEventListener('click' , deleteInput)
+
+numbers.forEach((button) =>
+button.addEventListener('click' , () => updateNumber(button.textContent))
+)
+
+operator.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function deleteInput() {
+    input.textContent = '';
+}
+
+
+function clearPage() {
+input.textContent = '';
+lastInput.textContent = '';
+firstNum = '';
+secondNum = '';
+firstOperator = '';
+}
+
+
+function updateNumber(number) {
+    
+    if(input.textContent === '') {}
+    input.textContent += number;
+
+}
+
+function setOperation(operator) {
+    firstNum = input.textContent;
+    currentOperation = operator
+    lastInput.textContent = `${firstNum} ${currentOperation}`
+    input.textContent = ''
+}
+
+function evaluate () {
+    secondNum = input.textContent
+    input.textContent = roundResult(operate(+firstNum, +secondNum, currentOperation));
+    lastInput.textContent = `${firstNum} ${currentOperation} ${secondNum}`
+    
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+  }
+
+function resetScreen(){
+    input.textContent = '';  
+
+}
 
 function addition(a,b){
     return a + b;
@@ -19,15 +86,14 @@ function division(a,b) {
     return a / b;
 }
 
-function operate(defaultNumberOne,defaultNumberTwo, firstOperand) {
+function operate(firstNum,secondNum, firstOperand) {
     if(firstOperand === '+'){
-        return defaultNumberOne + defaultNumberTwo;
+        return addition(firstNum,secondNum)
     } else if (firstOperand === '-'){
-        return defaultNumberOne - defaultNumberTwo;
+        return subtraction(firstNum,secondNum)
     } else if (firstOperand === '*'){
-        return defaultNumberOne * defaultNumberTwo;
+        return multiplication(firstNum,secondNum)
     } else if (firstOperand === '/'){
-        return defaultNumberOne / defaultNumberTwo;
+        return division(firstNum,secondNum)
     }
 }
-console.log(operate(5,5, '/'));
